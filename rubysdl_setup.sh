29f63973015ffdfb67eda387eaebcfd3dafd8ca5
Ryan Davis <ryand-ruby@zenspider.com>
2015-09-14 16:35:39 -0700

    Clean up rubysdl_setup.sh and re-add a self-test to it.

--- rubysdl_setup.sh
+++ rubysdl_setup.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-set -ex
+set -e
 
-brew uninstall libogg || true
-brew uninstall libvorbis || true
-brew uninstall libpng || true
+for pkg in sdl sdl_image sdl_ttf sdl_mixer sdl_gfx smpeg freetype libogg libvorbis libsmpeg libpng libtiff; do
+    brew uninstall $pkg || true
+done
 
-brew uninstall sdl || true
-brew uninstall sdl_ttf || true
-brew uninstall sdl_mixer || true
-brew uninstall sdl_image || true
-
-gem uninstall -ax rsdl || true
-gem uninstall -ax rubysdl || true
-gem uninstall -ax graphics || true
+for gem in graphics rubysdl rsdl; do
+    gem uninstall -ax $gem || true
+done
 
 # brew update
 
@@ -21,4 +16,11 @@ brew install sdl_mixer --with-smpeg
 brew install sdl_ttf
 brew install sdl_image
 
-# gem install graphics --pre
+if [ -f $0 ]; then
+    rake clean package
+    gem install pkg/graphics*.gem
+else
+    gem install graphics --pre
+fi
+
+rsdl -rgraphics -e 'Class.new(Graphics::Simulation) { def draw n; clear :white; text "hit escape to quit", 100, 100, :black; end; }.new(500, 250, 0, "Working!").run'
