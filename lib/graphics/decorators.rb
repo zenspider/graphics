1f55ec74a5fbfaaaa810856c1bb7202ecd45b90c
Ryan Davis <ryand-ruby@zenspider.com>
2016-11-22 19:14:24 -0800

    Added DrawGrid decorator.
    [git-p4: depot-paths = "//src/graphics/dev/": change = 11102]

--- lib/graphics/decorators.rb
+++ lib/graphics/decorators.rb
@@ -4,3 +4,14 @@ module ShowFPS
     fps n
   end
 end
+
+module DrawGrid
+  def pre_draw n
+    super
+
+    (0...w).step(self.class::GRID_WIDTH).each do |x|
+      hline x, :gray
+      vline x, :gray
+    end
+  end
+end
