4cc4edce0c7c93e08920d50ce46088374f5ad29f
Ryan Davis <ryand-ruby@zenspider.com>
2016-11-22 19:08:17 -0800

    Added collision detection to walker
    [git-p4: depot-paths = "//src/graphics/dev/": change = 10999]

--- examples/walker.rb
+++ examples/walker.rb
@@ -1,7 +1,5 @@
 #!/usr/local/bin/ruby -w
 
-# srand 42
-
 require "thingy"
 
 class Person < Body
@@ -10,6 +8,8 @@ class Person < Body
   D_A = 5.0
   D_M = 0.25
   M_M = 5.0
+
+  AA = SDL::Surface::TRANSFORM_AA
 
   attr_accessor :trail, :attack
 
@@ -40,7 +40,7 @@ class Person < Body
   end
 
   def draw
-    w.angle x, y, ga,   60, :red
+    trail.draw
 
     if attack then
       w.angle x, y, a-45, 50, :yellow
@@ -48,31 +48,52 @@ class Person < Body
       w.angle x, y, a+45, 50, :yellow
     end
 
-    w.circle x, y, 5, :white, :filled
+    w.angle x, y, ga,   60, :red
 
-    trail.draw
+    # the blit looks HORRIBLE when rotated... dunno why
+    w.blit w.body_img, x, y, 0, AA
   end
 
   def turn_towards_goal
     turn a.relative_angle(ga, D_A)
   end
 
+  def change_goal
+    self.ga = a + random_turn(180)
+  end
+
   def possibly_change_goal
-    self.ga = a + random_turn(180) if ga.close_to?(a) && 1 =~ 25
+    change_goal if ga.close_to?(a) && 1 =~ 25
+  end
+
+  def collide_with? other
+    w.cmap.collision_check(x, y, w.cmap, other.x, other.y) != nil
+  end
+
+  def collide
+    self.a = (a + 180).degrees
+    change_goal
   end
 end
 
 class WalkerThingy < Thingy
-  attr_accessor :ps
+  attr_accessor :ps, :body_img, :cmap
 
   def initialize
     super 850, 850, 16, "Walker"
 
     self.ps = populate Person
+
+    self.body_img = sprite 20, 20 do
+      circle 10, 10, 5, :white, :filled
+    end
+
+    self.cmap = body_img.make_collision_map
   end
 
   def update n
     ps.each(&:update)
+    detect_collisions(ps).each(&:collide)
   end
 
   def draw n
@@ -80,6 +101,14 @@ class WalkerThingy < Thingy
 
     ps.each(&:draw)
     fps n
+  end
+
+  def detect_collisions(sprites)
+    collisions = []
+    sprites.combination(2).each do |a, b|
+      collisions << a << b if a.collide_with? b
+    end
+    collisions.uniq
   end
 end
 
