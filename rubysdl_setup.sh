cdb384b905bcc1e358b62d04077f35acf3751a93
Ryan Davis <ryand-ruby@zenspider.com>
2016-11-22 19:12:58 -0800

    Updated rubysdl_setup.sh to be more correct. Not quite complete yet.
    [git-p4: depot-paths = "//src/graphics/dev/": change = 11075]

--- rubysdl_setup.sh
+++ rubysdl_setup.sh
@@ -10,22 +10,15 @@ brew uninstall sdl_ttf || true
 brew uninstall sdl_mixer || true
 brew uninstall sdl_image || true
 
+gem uninstall -ax rsdl || true
+gem uninstall -ax rubysdl || true
+gem uninstall -ax graphics || true
+
 # brew update
 
-brew install libogg
-brew install libvorbis
-brew install libpng
-
 brew install sdl
-brew install sdl_mixer
+brew install sdl_mixer --with-smpeg
 brew install sdl_ttf
 brew install sdl_image
 
-gem uninstall -ax rsdl || true
-gem install rsdl
-
-ruby -r sdl -e 'p [:ttf, SDL.constants.include?(:TTF)]'
-ruby -r sdl -e 'p [:mixer, SDL.constants.include?(:Mixer)]'
-ruby -r sdl -e 'p [:sge, SDL.respond_to?(:auto_lock)]'
-
-rsdl -r sdl -e 'SDL.init(SDL::INIT_EVERYTHING); SDL.set_video_mode(640, 480, 16, SDL::SWSURFACE); sleep(1)'
+# gem install graphics --pre
